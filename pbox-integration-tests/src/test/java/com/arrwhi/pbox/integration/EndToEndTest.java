63939c68a303c2cd1cc169c021526b768190552f
arran <arrwhidev@gmail.com>
2017-01-29 13:18:21 +0000

    WIP - working on an integration test suite so I can test various buffer sizes

--- pbox-integration-tests/src/test/java/com/arrwhi/pbox/integration/EndToEndTest.java
+++ pbox-integration-tests/src/test/java/com/arrwhi/pbox/integration/EndToEndTest.java
@@ -1,14 +1,23 @@
 package com.arrwhi.pbox.integration;
 
+import com.arrwhi.pbox.RandomTestUtils;
 import com.arrwhi.pbox.client.Client;
+import com.arrwhi.pbox.client.index.difference.DifferenceType;
 import com.arrwhi.pbox.server.Server;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 /**
  * Created by arran on 28/01/17.
@@ -16,7 +25,7 @@ import java.util.concurrent.CountDownLatch;
 public class EndToEndTest {
 
     Server server;
-    Channel serverChannel;
+    ServerHandler serverHandler;
 
     Client client;
     Channel clientChannel;
@@ -25,13 +34,10 @@ public class EndToEndTest {
     public void setUp() throws Exception {
         final CountDownLatch connectionLatch = new CountDownLatch(1);
 
+        serverHandler = new ServerHandler();
         server = new Server();
-        ChannelFuture serverFuture = server.start(9991, Arrays.asList(
-            new ServerHandler()
-        ));
-        serverChannel = serverFuture.channel();
+        ChannelFuture serverFuture = server.start(9991, Arrays.asList(serverHandler));
         serverFuture.awaitUninterruptibly();
-        System.out.println("Started server...");
 
         client = new Client();
         ChannelFuture clientFuture = client.start("localhost", 9991, new ClientHandler());
@@ -41,23 +47,28 @@ public class EndToEndTest {
         clientFuture.awaitUninterruptibly();
 
         connectionLatch.await();
-        System.out.println("Started client...");
     }
 
     @Test
-    public void testing() throws Exception {
-//        final CountDownLatch messageLatch = new CountDownLatch(1);
-//
-//        byte[] packet = new byte[3];
-//        packet[0] = 'a';
-//        packet[0] = 'b';
-//        packet[0] = 'c';
-//        clientChannel.writeAndFlush(packet);
-//        System.out.println("Wrote msg to server.");
-//
-//        messageLatch.await();
+    public void shouldRecvSentMessage() throws Exception {
+        final int numMsg = 2;
+        final CountDownLatch messageLatch = new CountDownLatch(numMsg);
+        serverHandler.setLatch(messageLatch);
+
+        ByteBuf buf1 = RandomTestUtils.randomByteBuf(16);
+        ByteBuf buf2 = RandomTestUtils.randomByteBuf(16);
+        clientChannel.writeAndFlush(buf1);
+        clientChannel.writeAndFlush(buf2);
+
+        messageLatch.await();
+
+        assertThat(serverHandler.getRecvedMsgs().size(), equalTo(numMsg));
+        // TODO: Assert that sent buffers are equal.
+        // assertThat(serverHandler.getRecvedMsgs().get(0).compareTo(buf1), equalTo(0));
     }
 }
+
+// TODO: Tidy all this junk up.
 
 class TestFutureListener implements ChannelFutureListener {
 
@@ -70,7 +81,6 @@ class TestFutureListener implements ChannelFutureListener {
 
     @Override
     public void operationComplete(ChannelFuture future) {
-        System.out.println("Client successfully connected to server!");
         channel = future.channel();
         latch.countDown();
     }
@@ -79,68 +89,29 @@ class TestFutureListener implements ChannelFutureListener {
 class ClientHandler extends ChannelInboundHandlerAdapter {
 
     public void channelRead(final ChannelHandlerContext ctx, Object msg) {
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
+        // TODO
     }
 }
 
 class ServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(1);
-        super.channelUnregistered(ctx);
-    }
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(2);
-        super.channelActive(ctx);
-    }
-
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(3);
-        super.channelInactive(ctx);
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(4);
-        super.channelReadComplete(ctx);
-    }
-
-    @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        System.out.println(5);
-        super.userEventTriggered(ctx, evt);
-    }
-
-    @Override
-    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(6);
-        super.channelWritabilityChanged(ctx);
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        System.out.println(7);
-        super.exceptionCaught(ctx, cause);
-    }
-
-    @Override
-    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(8);
-        super.channelRegistered(ctx);
-    }
+    private CountDownLatch latch;
+    private List<ByteBuf> recvedMsgs = new ArrayList<>();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println(9);
         super.channelRead(ctx, msg);
+        recvedMsgs.add((ByteBuf)msg);
+        if (latch != null) {
+            latch.countDown();
+        }
+    }
+
+    public void setLatch(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public List<ByteBuf> getRecvedMsgs() {
+        return recvedMsgs;
     }
 }
