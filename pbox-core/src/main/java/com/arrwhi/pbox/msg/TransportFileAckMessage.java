9a3871b2eec0e7ed126a64bc3338748ea35bf435
arran <arrwhidev@gmail.com>
2017-07-29 18:37:10 +0100

    TransportFileAckMessage now supports meta data at a protocol level.

--- pbox-core/src/main/java/com/arrwhi/pbox/msg/TransportFileAckMessage.java
+++ pbox-core/src/main/java/com/arrwhi/pbox/msg/TransportFileAckMessage.java
@@ -4,9 +4,14 @@ package com.arrwhi.pbox.msg;
  * Transport File Ack Message
  * 
  * 2 byte message type      <from Message>
+ * 4 byte metadata length   <from MessageWithMetaData>
+ * n byte metadata          <from MessageWithMetaData>
  */
-public class TransportFileAckMessage extends Message {
+public class TransportFileAckMessage extends MessageWithMetaData {
     public TransportFileAckMessage() {
-        super(MessageFactory.TRANSPORT_FILE_ACK);
+        this(null);
+    }
+    public TransportFileAckMessage(byte[] metadata) {
+        super(MessageFactory.TRANSPORT_FILE_ACK, metadata);
     }
 }
